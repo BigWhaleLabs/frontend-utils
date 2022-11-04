89c70605cf3144668a1e899b55340034d05ce300
Daniil <dpancov64@gmail.com>
2022-11-04 21:53:10 +0300

    lets put example error in separate file

---
+++ src/helpers/exapleErrors.ts
@@ -0,0 +1,54 @@
+import { ErrorList } from '../types/ErrorList'
+
+export const exampleErrors = {
+  axiosError: {
+    error: {
+      message: 'Request failed with status code 400',
+      name: 'AxiosError',
+      config: {
+        transitional: {
+          silentJSONParsing: true,
+          forcedJSONParsing: true,
+          clarifyTimeoutError: false,
+        },
+        transformRequest: [null],
+        transformResponse: [null],
+        timeout: 0,
+        xsrfCookieName: 'XSRF-TOKEN',
+        xsrfHeaderName: 'X-XSRF-TOKEN',
+        maxContentLength: -1,
+        maxBodyLength: -1,
+        env: {
+          FormData: null,
+        },
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+        method: 'post',
+        url: 'https://verify.sealcred.xyz/v0.2.1/verify/contract-metadata',
+        data: '{"tokenAddress":"0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85","network":"Mainnet"}',
+      },
+      code: 'ERR_BAD_REQUEST',
+      status: 400,
+    },
+    shouldDisplay: 'Request failed with status code 400',
+  },
+  insufficientFunds: {
+    error: {
+      code: -32000,
+      message:
+        'err: insufficient funds for gas * price + value: address 0x0000000000000000000000000000000000000000 have 2130760518416292149 want 23675865423000000000 (supplied gas 500000000)',
+    },
+    shouldDisplay: ErrorList.gsnError,
+  },
+  failedToRelayCall: {
+    error: 'Error: Failed to relay call. Results:',
+    shouldDisplay: ErrorList.gsnError,
+  },
+  unhandledRejection: {
+    error:
+      "Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'error')",
+    shouldDisplay: ErrorList.gsnError,
+  },
+}
